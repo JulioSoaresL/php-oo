ab92b24db9dee809215c8de6f854e9005167cdc4
julioSoaresL <juliosoareslima@outlook.com>
2022-08-20 22:16:52 -0300

    metodos e atributos

---
+++ src/ContaBancaria.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+class ContaBancaria
+{
+    public string $banco = "NuBank";
+    public string $nomeTitular = "Júlio Soares";
+    public string $numeroAgencia = "8244";
+    public string $numeroConta = "57648-69";
+    public float $saldo = 0;
+
+    public function exibirDadosConta(): array
+    {
+        return [
+            'banco' => $this->banco,
+            'nomeTitular' => $this->nomeTitular,
+            'numeroAgencia' => $this->numeroAgencia,
+            'numeroConta' => $this->numeroConta,
+            'saldo' => $this->saldo,
+        ];
+    }
+}
