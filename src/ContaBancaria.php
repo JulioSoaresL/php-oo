ab8e42dd9317ddf88da8366759036515bd135cfd
julioSoaresL <juliosoareslima@outlook.com>
2022-08-21 00:08:35 -0300

    adicionando classe e instancias com construtor

--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -4,11 +4,42 @@ namespace App;
 
 class ContaBancaria
 {
-    public string $banco = "NuBank";
-    public string $nomeTitular = "Júlio Soares";
-    public string $numeroAgencia = "8244";
-    public string $numeroConta = "57648-69";
-    public float $saldo = 0;
+    private string $banco;
+    private string $nomeTitular;
+    private string $numeroAgencia;
+    private string $numeroConta;
+    private float $saldo;
+
+    public function __construct(
+        string $banco,
+        string $nomeTitular,
+        string $numeroAgencia,
+        string $numeroConta,
+        float $saldo
+    )
+    {
+        $this->banco = $banco;
+        $this->nomeTitular = $nomeTitular;
+        $this->numeroAgencia = $numeroAgencia;
+        $this->numeroConta = $numeroConta;
+        $this->saldo = $saldo;
+    }
+
+    public function depositar(float $valor): string
+    {
+        $this->saldo += $valor;
+        return 'Depósito de R$ '.number_format($valor, 2, ',', '').' realizado';
+    }
+
+    public function sacar(float $valor): string
+    {
+        $this->saldo-=$valor;
+        return 'Saque de R$ '.number_format($valor,2, ',', '').' realizado';
+    }
+    public function obterSaldo(): string
+    {
+        return 'Saldo Atual: R$ '.number_format($this->saldo, 2, ',', '');
+    }
 
     public function exibirDadosConta(): array
     {
@@ -20,4 +51,18 @@ class ContaBancaria
             'saldo' => $this->saldo,
         ];
     }
+
+    public function exibirNomeTitular()
+    {
+        return [
+            'nomeTitular' => $this->nomeTitular,
+            'contaTitular' =>$this->numeroConta,
+        ];
+    }
+    
+    public function getBanco(): string
+    {
+        return $this->banco;
+    }
+
 }   
